b30e1d9dcd8bf428048149616ab36fae3c08b0d9
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-05-16 20:00:57 +0530

    modified:   components/Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,21 @@
-import { ChevronDownIcon, HomeIcon } from '@heroicons/react/16/solid';
 import Image from 'next/image';
-import React from 'react'
+import React from 'react';
+
+import {
+  SparklesIcon,
+  VideoCameraIcon,
+  ChatBubbleLeftIcon
+} from '@heroicons/react/24/outline';
+import {
+  ChevronDownIcon,
+  HomeIcon,
+  MagnifyingGlassIcon
+} from '@heroicons/react/16/solid';
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
 
 function Header() {
   return (
-    <div className="sticky top-0 flex bg-white px-4 py-2 shadow-sm">
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0">
         <Image
           src="https://links.papareact.com/fqy"
@@ -13,10 +24,31 @@ function Header() {
           objectFit="contain"
         />
       </div>
-      <div className="mx-7 flex items-center">
+      <div className="mx-7 flex items-center xl:min-w-[300px]">
         <HomeIcon className="h-5 w-5" />
-        <p>Home</p>
+        <p className="flex-1 ml-2 hidden lg:inline">Home</p>
         <ChevronDownIcon className="h-5 w-5" />
+      </div>
+
+      {/* Search box */}
+      <form className="flex flex-1 items-center space-x-2 rounded-sm border border-gray-200 bg-gray-100 px-3 py-1">
+        <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+        <input
+          type="text"
+          placeholder="Search Reddit"
+          className='flex-1 bg-transparent outline-none'
+        />
+        <button type="submit" hidden />
+      </form>
+
+      <div>
+        <SparklesIcon className="icon" />
+        <GlobeAltIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="h-10 border border-gray-100" />
+        <ChatBubbleLeftIcon className="icon" />
+        <GlobeAltIcon className="icon" />
+        <VideoCameraIcon className="icon" />
       </div>
     </div>
   )
